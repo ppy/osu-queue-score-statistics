febdfb4f259d112f8d0b2c1c8e33bfbaa8686713
Dean Herbert <pe@ppy.sh>
2021-06-14 18:52:50 +0900

    Add transactional safety

--- osu.Server.Queues.ScoreStatisticsProcessor/ScoreStatisticsProcessor.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/ScoreStatisticsProcessor.cs
@@ -17,6 +17,7 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor
         protected override void ProcessResult(ScoreItem item)
         {
             using (var db = GetDatabaseConnection())
+            using (var transaction = db.BeginTransaction())
             {
                 if (item.ruleset_id > 0)
                 {
@@ -29,7 +30,7 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor
                     Console.WriteLine($"Item {item} already processed, rolling back before reapplying");
 
                     // if required, we can rollback any previous version of processing then reapply with the latest.
-                    db.Execute("UPDATE osu_user_stats SET playcount = playcount - 1 WHERE user_id = @user_id", item);
+                    db.Execute("UPDATE osu_user_stats SET playcount = playcount - 1 WHERE user_id = @user_id", item, transaction);
                 }
 
                 Console.WriteLine($"Processing score {item}");
@@ -37,9 +38,11 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor
                 db.Execute("INSERT INTO osu.osu_user_stats "
                            + "(user_id, count300, count100, count50, countMiss, accuracy_total, accuracy_count, accuracy, playcount, ranked_score, total_score, x_rank_count, xh_rank_count, s_rank_count, sh_rank_count, a_rank_count, `rank`, level, replay_popularity, fail_count, exit_count, max_combo, country_acronym, rank_score, rank_score_index, accuracy_new, last_update, last_played, total_seconds_played) "
                            + "VALUES (@user_id, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 0, 0, 0, 1, 0, 0, 0, DEFAULT, 0, DEFAULT, 0, 0, 0, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 0, 0, 0, DEFAULT, DEFAULT, DEFAULT) "
-                           + "ON DUPLICATE KEY UPDATE playcount = playcount + 1", item);
+                           + "ON DUPLICATE KEY UPDATE playcount = playcount + 1", item, transaction);
 
-                db.Execute("UPDATE solo_scores SET processed_at = NOW() WHERE id = @id", item);
+                db.Execute("UPDATE solo_scores SET processed_at = NOW() WHERE id = @id", item, transaction);
+
+                transaction.Commit();
             }
         }
     }
