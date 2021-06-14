9ceaa895b5858137f7012a1df0f3cd43e58e8048
Dean Herbert <pe@ppy.sh>
2021-06-14 18:51:57 +0900

    Add insert logic for cases where stats row doesn't exist

--- osu.Server.Queues.ScoreStatisticsProcessor/ScoreStatisticsProcessor.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/ScoreStatisticsProcessor.cs
@@ -34,7 +34,11 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor
 
                 Console.WriteLine($"Processing score {item}");
 
-                db.Execute("UPDATE osu_user_stats SET playcount = playcount + 1 WHERE user_id = @user_id", item);
+                db.Execute("INSERT INTO osu.osu_user_stats "
+                           + "(user_id, count300, count100, count50, countMiss, accuracy_total, accuracy_count, accuracy, playcount, ranked_score, total_score, x_rank_count, xh_rank_count, s_rank_count, sh_rank_count, a_rank_count, `rank`, level, replay_popularity, fail_count, exit_count, max_combo, country_acronym, rank_score, rank_score_index, accuracy_new, last_update, last_played, total_seconds_played) "
+                           + "VALUES (@user_id, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 0, 0, 0, 1, 0, 0, 0, DEFAULT, 0, DEFAULT, 0, 0, 0, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 0, 0, 0, DEFAULT, DEFAULT, DEFAULT) "
+                           + "ON DUPLICATE KEY UPDATE playcount = playcount + 1", item);
+
                 db.Execute("UPDATE solo_scores SET processed_at = NOW() WHERE id = @id", item);
             }
         }
