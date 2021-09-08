10aea2bf8c205e18042312af3ddcc0f772e696f1
smoogipoo <smoogipoo@smgi.me>
2021-09-08 20:36:41 +0900

    Remove temporaries

--- osu.Server.Queues.ScoreStatisticsProcessor/Processors/PerformanceProcessor.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Processors/PerformanceProcessor.cs
@@ -37,14 +37,10 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Processors
             // Todo: We shouldn't be using legacy mods, but this requires difficulty calculation to be performed in-line.
             LegacyMods legacyModValue = LegacyModsHelper.MaskRelevantMods(ruleset.ConvertToLegacyMods(mods));
 
-            var beatmap = conn.QuerySingleOrDefault<Beatmap?>("SELECT * FROM osu_beatmaps WHERE beatmap_id = @BeatmapId", new
+            var beatmap = conn.QuerySingle<Beatmap>("SELECT * FROM osu_beatmaps WHERE beatmap_id = @BeatmapId", new
             {
                 BeatmapId = score.beatmap_id
             });
-
-            // Todo: REMOVE THIS. This should never be the case (testing-only).
-            if (beatmap == null)
-                return;
 
             var rawDifficultyAttribs = conn.Query<BeatmapDifficultyAttribute>(
                 "SELECT * FROM osu_beatmap_difficulty_attribs WHERE beatmap_id = @BeatmapId AND mode = @RulesetId AND mods = @ModValue", new
@@ -52,14 +48,9 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Processors
                     BeatmapId = score.beatmap_id,
                     RulesetId = score.ruleset_id,
                     ModValue = (uint)legacyModValue
-                })?.ToArray();
-
-            // Todo: REMOVE THIS. This should never be the case (testing-only).
-            if (rawDifficultyAttribs == null || rawDifficultyAttribs.Length == 0)
-                return;
+                }).ToArray();
 
             var difficultyAttributes = rawDifficultyAttribs.ToDictionary(a => (int)a.attrib_id).Map(score.ruleset_id, beatmap);
-
             var performanceCalculator = ruleset.CreatePerformanceCalculator(difficultyAttributes, scoreInfo);
             var performance = performanceCalculator.Calculate();
 
