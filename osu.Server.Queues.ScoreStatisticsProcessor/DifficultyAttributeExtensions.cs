d9466f3936a28516b31311bb5e0ff34f7d3bbaf7
smoogipoo <smoogipoo@smgi.me>
2021-09-29 13:28:06 +0900

    Remove other unused difficulty attribute mapping method

--- osu.Server.Queues.ScoreStatisticsProcessor/DifficultyAttributeExtensions.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/DifficultyAttributeExtensions.cs
@@ -61,45 +61,5 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor
                     throw new ArgumentException($"Invalid ruleset ({rulesetId}).", nameof(rulesetId));
             }
         }
-
-        public static IEnumerable<(int id, object value)> Map(this DifficultyAttributes attributes)
-        {
-            switch (attributes)
-            {
-                case OsuDifficultyAttributes osu:
-                    yield return (1, osu.AimStrain);
-                    yield return (3, osu.SpeedStrain);
-                    yield return (5, osu.OverallDifficulty);
-                    yield return (7, osu.ApproachRate);
-                    yield return (9, osu.MaxCombo);
-                    yield return (11, attributes.StarRating);
-                    yield return (17, osu.FlashlightRating);
-
-                    break;
-
-                case TaikoDifficultyAttributes taiko:
-                    yield return (9, taiko.MaxCombo);
-                    yield return (11, attributes.StarRating);
-                    yield return (13, taiko.GreatHitWindow);
-
-                    break;
-
-                case CatchDifficultyAttributes @catch:
-                    // Todo: Catch should not output star rating in the 'aim' attribute.
-                    yield return (1, @catch.StarRating);
-                    yield return (7, @catch.ApproachRate);
-                    yield return (9, @catch.MaxCombo);
-
-                    break;
-
-                case ManiaDifficultyAttributes mania:
-                    // Todo: Mania doesn't output MaxCombo attribute for some reason.
-                    yield return (11, attributes.StarRating);
-                    yield return (13, mania.GreatHitWindow);
-                    yield return (15, mania.ScoreMultiplier);
-
-                    break;
-            }
-        }
     }
 }
