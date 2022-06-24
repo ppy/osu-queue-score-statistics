553e5a1cc6fb20da67603a7107b5efc276dda5a5
Dean Herbert <pe@ppy.sh>
2022-06-24 13:00:35 +0900

    Fix cancellation via ctrl+c not gracefully exiting

--- osu.Server.Queues.ScorePump/Program.cs
+++ osu.Server.Queues.ScorePump/Program.cs
@@ -1,4 +1,6 @@
-﻿using McMaster.Extensions.CommandLineUtils;
+﻿using System;
+using System.Threading;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace osu.Server.Queues.ScorePump
 {
@@ -10,9 +12,17 @@ namespace osu.Server.Queues.ScorePump
     [Subcommand(typeof(ImportHighScores))]
     public class Program
     {
+        private static readonly CancellationTokenSource cts = new CancellationTokenSource();
+
         public static void Main(string[] args)
         {
-            CommandLineApplication.Execute<Program>(args);
+            Console.CancelKeyPress += delegate
+            {
+                Console.WriteLine("Cancellation requested!");
+                cts.Cancel();
+            };
+
+            CommandLineApplication.ExecuteAsync<Program>(args, cts.Token);
         }
 
         public int OnExecute(CommandLineApplication app)
