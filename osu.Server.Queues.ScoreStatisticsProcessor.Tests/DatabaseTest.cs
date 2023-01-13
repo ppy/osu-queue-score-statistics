1baf3a0d9512b176b8abd3beb33d2b9a9be0a1ba
Dan Balasescu <smoogipoo@smgi.me>
2023-01-13 17:51:21 +0900

    Disable inline difficulty calculation in tests

--- osu.Server.Queues.ScoreStatisticsProcessor.Tests/DatabaseTest.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor.Tests/DatabaseTest.cs
@@ -39,6 +39,7 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
         protected DatabaseTest()
         {
             Environment.SetEnvironmentVariable("SCHEMA", "1");
+            Environment.SetEnvironmentVariable("INLINE_DIFFICULTY_CALCULATION", "0");
 
             Processor = new ScoreStatisticsProcessor();
             Processor.Error += processorOnError;
