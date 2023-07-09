597edd754acd4b9ff9b8d4d62ed1eacae153b2ae
Dean Herbert <pe@ppy.sh>
2023-07-10 02:34:40 +0900

    Add command to recalculate playlist scores to OLD standardised scoring

--- osu.Server.Queues.ScorePump/Program.cs
+++ osu.Server.Queues.ScorePump/Program.cs
@@ -8,6 +8,7 @@ namespace osu.Server.Queues.ScorePump
     [Command]
     [Subcommand(typeof(QueueCommands))]
     [Subcommand(typeof(PerformanceCommands))]
+    [Subcommand(typeof(RecalculatePlaylistTotalScores))]
     [Subcommand(typeof(RecalculateScoresCommand))]
     public class Program
     {
