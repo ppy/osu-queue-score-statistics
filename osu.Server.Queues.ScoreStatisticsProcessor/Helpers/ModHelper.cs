46e7ab6882eed40ec08c83d13792e979ad92ffd0
substanc3-dev <jano4varga@gmail.com>
2024-01-03 23:26:56 +0100

    Change difficulty reduction mod check to cover more cases

--- osu.Server.Queues.ScoreStatisticsProcessor/Helpers/ModHelper.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Helpers/ModHelper.cs
@@ -7,21 +7,27 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Helpers
     {
         public static bool IsDifficultyReductionMod(this Mod mod)
         {
+            if (mod.Type == ModType.DifficultyReduction || mod.Type == ModType.Automation)
+                return true;
+
             switch (mod)
             {
-                case ModEasy:
-                case ModNoFail:
-                case ModHalfTime:
-                case OsuModSpunOut:
-                    return true;
-
                 // Difficulty adjust can be used to decrease the difficulty of the map,
                 // so let's consider it difficulty reducing for now.
                 case ModDifficultyAdjust difficultyAdjust:
                     return !difficultyAdjust.UsesDefaultConfiguration;
 
-                default:
+                case OsuModTargetPractice:
+                    return true;
+
+                // Allow the use of certain Fun mods that are not considered difficulty-reducing
+                case ModMuted:
+                case ModNoScope:
                     return false;
+
+                // Disallow the use of the other Fun mods
+                default:
+                    return mod.Type == ModType.Fun;
             }
         }
     }
