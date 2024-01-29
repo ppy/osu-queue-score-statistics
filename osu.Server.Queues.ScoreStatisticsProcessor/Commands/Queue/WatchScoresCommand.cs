4db6073a97290d2485102c8e87658c4757b8f539
Dean Herbert <pe@ppy.sh>
2024-01-30 05:09:55 +0900

    Remove unused using

--- osu.Server.Queues.ScoreStatisticsProcessor/Commands/Queue/WatchScoresCommand.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Commands/Queue/WatchScoresCommand.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
