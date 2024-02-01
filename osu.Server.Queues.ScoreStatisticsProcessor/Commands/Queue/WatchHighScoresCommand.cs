570b8b8121f585fad9befcdbb80fc1080bc2485d
Dean Herbert <pe@ppy.sh>
2024-02-01 20:51:16 +0900

    Stop processing at the timed-out entry

--- osu.Server.Queues.ScoreStatisticsProcessor/Commands/Queue/WatchHighScoresCommand.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Commands/Queue/WatchHighScoresCommand.cs
@@ -161,6 +161,9 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Queue
                         if (pendingProcessingWaitTime > 300000)
                         {
                             Console.WriteLine($"Importing score which was not processed due to timeout (score_id: {pendingProcessing.score_id} queue_id: {pendingProcessing.queue_id})");
+
+                            // only process up to the timed-out entry.
+                            highScores = highScores.Where(s => s.queue_id <= pendingProcessing.score_id).ToArray();
                         }
                         else
                         {
