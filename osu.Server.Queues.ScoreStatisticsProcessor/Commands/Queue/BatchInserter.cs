c5107a5859735137638bf1cbcfd898c53e1973b6
Dean Herbert <pe@ppy.sh>
2024-03-13 15:22:54 +0800

    Delete score pins when cleaning up old user high scores during high score import

--- osu.Server.Queues.ScoreStatisticsProcessor/Commands/Queue/BatchInserter.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Commands/Queue/BatchInserter.cs
@@ -103,7 +103,15 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Queue
                         }
 
                         using (var conn = DatabaseAccess.GetConnection())
-                            conn.Execute("DELETE FROM scores WHERE id = @new_id", new { highScore.new_id });
+                        {
+                            conn.Execute("DELETE FROM score_pins WHERE score_type = 'solo_score' AND user_id = @new_user_id AND id = @new_id;"
+                                         + "DELETE FROM scores WHERE id = @new_id", new
+                            {
+                                highScore.new_id,
+                                highScore.new_user_id,
+                            });
+                        }
+
                         ElasticScoreItems.Add(new ElasticQueuePusher.ElasticScoreItem { ScoreId = (long)highScore.new_id });
 
                         Interlocked.Increment(ref TotalDeleteCount);
