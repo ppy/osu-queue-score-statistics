4fd6b8ed327b9dd75f4b0be1d0f7bfb4f00bbd1a
Dean Herbert <pe@ppy.sh>
2024-03-18 21:59:02 +0800

    Add a command to update user totals for a scoped sql range

---
+++ osu.Server.Queues.ScoreStatisticsProcessor/Commands/Performance/UserTotals/UpdateUserTotalsFromSqlCommand.cs
@@ -0,0 +1,41 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using JetBrains.Annotations;
+using McMaster.Extensions.CommandLineUtils;
+using osu.Server.QueueProcessor;
+using osu.Server.Queues.ScoreStatisticsProcessor.Helpers;
+
+namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Performance.UserTotals
+{
+    [Command("sql", Description = "Updates the total PP of scoped users.")]
+    public class UpdateUserTotalsFromSqlCommand : PerformanceCommand
+    {
+        [UsedImplicitly]
+        [Required]
+        [Argument(0, Description = "Specify a custom query to limit the scope of users to process.")]
+        public string? Query { get; set; }
+
+        protected override async Task<int> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            using var db = DatabaseAccess.GetConnection();
+
+            LegacyDatabaseHelper.RulesetDatabaseInfo databaseInfo = LegacyDatabaseHelper.GetRulesetSpecifics(RulesetId);
+
+            Console.WriteLine($"Fetching scoped users ({Query})...");
+
+            int[] userIds = (await db.QueryAsync<int>($"SELECT `user_id` FROM {databaseInfo.UserStatsTable} WHERE {Query}")).ToArray();
+
+            Console.WriteLine($"Fetched {userIds.Length} users");
+
+            await ProcessUserTotals(userIds, cancellationToken);
+            return 0;
+        }
+    }
+}
