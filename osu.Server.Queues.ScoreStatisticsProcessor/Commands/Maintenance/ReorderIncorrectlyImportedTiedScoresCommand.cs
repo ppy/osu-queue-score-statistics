e6e6eed92727e2f89f150ed8b9a20172bb2aa4f7
Dean Herbert <pe@ppy.sh>
2024-04-02 17:53:18 +0800

    Optimise by using old tables which actually have indices we can rely on

--- osu.Server.Queues.ScoreStatisticsProcessor/Commands/Maintenance/ReorderIncorrectlyImportedTiedScoresCommand.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Commands/Maintenance/ReorderIncorrectlyImportedTiedScoresCommand.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Dapper;
 using McMaster.Extensions.CommandLineUtils;
 using osu.Server.QueueProcessor;
+using osu.Server.Queues.ScoreStatisticsProcessor.Helpers;
 using osu.Server.Queues.ScoreStatisticsProcessor.Models;
 
 namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Maintenance
@@ -38,6 +39,8 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Maintenance
             Console.WriteLine($"Verifying tied score orders for ruleset {RulesetId}");
             Console.WriteLine($"Indexing to elasticsearch queue(s) {elasticQueueProcessor.ActiveQueues}");
 
+            var rulesetSpecifics = LegacyDatabaseHelper.GetRulesetSpecifics(RulesetId);
+
             int totalReordered = 0;
 
             if (DryRun)
@@ -49,12 +52,19 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Maintenance
 
             for (int i = 0; i < beatmaps.Length; i++)
             {
-                dynamic beatmap = beatmaps[i];
-                Console.WriteLine($"Processing {beatmap.beatmap_id}...");
+                if (cancellationToken.IsCancellationRequested)
+                    break;
 
+                dynamic beatmap = beatmaps[i];
+
+                if (i % 1000 == 0)
+                    Console.WriteLine($"Processing {beatmap.beatmap_id}...");
+
+                // Use old scores table because the lookup is faster.
                 ulong[] topScoresCheck =
                     (await conn.QueryAsync<ulong>(
-                        $"SELECT total_score FROM scores WHERE beatmap_id = {beatmap.beatmap_id} and ruleset_id = {RulesetId} AND preserve = 1 AND legacy_score_id IS NOT NULL ORDER BY total_score DESC LIMIT 2", commandTimeout: 60000))
+                        $"SELECT score FROM {rulesetSpecifics.HighScoreTable} WHERE beatmap_id = {beatmap.beatmap_id} AND hidden = 0 ORDER BY score DESC LIMIT 2",
+                        commandTimeout: 60000))
                     .ToArray();
 
                 if (topScoresCheck.Length != 2 || topScoresCheck[0] != topScoresCheck[1])
@@ -62,18 +72,22 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Maintenance
 
                 ulong topScore = topScoresCheck[0];
 
-                Console.WriteLine("Has tied scores, checking order...");
+                Console.Write($"{beatmap.beatmap_id} has tied scores, checking order... ");
 
                 var topScores = (await conn.QueryAsync<SoloScore>(
-                        $"SELECT * FROM scores WHERE beatmap_id = {beatmap.beatmap_id} and ruleset_id = {RulesetId} AND preserve = 1 AND total_score = {topScore} ORDER BY id", commandTimeout: 60000))
+                        $"SELECT id, legacy_score_id FROM scores WHERE beatmap_id = {beatmap.beatmap_id} and ruleset_id = {RulesetId} AND preserve = 1 AND legacy_score_id IN (SELECT score_id FROM {rulesetSpecifics.HighScoreTable} WHERE beatmap_id = {beatmap.beatmap_id} AND hidden = 0 AND score = {topScore}) ORDER BY id",
+                        commandTimeout: 60000))
                     .ToArray();
 
                 var topScoresSorted = topScores.OrderBy(s => s.legacy_score_id).ToArray();
 
                 if (topScores.SequenceEqual(topScoresSorted))
+                {
+                    Console.WriteLine("OK");
                     continue;
+                }
 
-                Console.WriteLine("Requires reordering...");
+                Console.WriteLine("FAIL");
 
                 if (!DryRun)
                 {
