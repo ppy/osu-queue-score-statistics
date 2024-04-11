fe823514a3f03a48acf1ca5b5146bb4fa9ad9472
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-04-11 08:51:20 +0200

    Add extended test coverage of existing mark non-preserved cases

--- osu.Server.Queues.ScoreStatisticsProcessor.Tests/MarkNonPreservedScoresCommandTest.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor.Tests/MarkNonPreservedScoresCommandTest.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Threading.Tasks;
+using Dapper;
 using osu.Server.Queues.ScoreStatisticsProcessor.Commands.Maintenance;
 using osu.Server.Queues.ScoreStatisticsProcessor.Models;
 using Xunit;
@@ -15,6 +16,11 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
         public MarkNonPreservedScoresCommandTest()
         {
             beatmap = AddBeatmap();
+
+            using var db = Processor.GetDatabaseConnection();
+
+            db.Execute("DELETE FROM `multiplayer_playlist_item_scores`");
+            db.Execute("TRUNCATE TABLE `score_pins`");
         }
 
         [Fact]
@@ -46,6 +52,64 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
             WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 1", 2, CancellationToken);
             WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 0", 1, CancellationToken);
             WaitForDatabaseState("SELECT `preserve` FROM `scores` WHERE `id` = 3", false, CancellationToken);
+        }
+
+        [Fact]
+        public async Task NonBestScoresRemainPreservedIfPinned()
+        {
+            using var db = Processor.GetDatabaseConnection();
+
+            SetScoreForBeatmap(beatmap.beatmap_id, s =>
+            {
+                s.Score.id = 1;
+                s.Score.total_score = 800_000;
+                s.Score.pp = 95;
+            });
+            SetScoreForBeatmap(beatmap.beatmap_id, s =>
+            {
+                s.Score.id = 2;
+                s.Score.total_score = 500_000;
+                s.Score.pp = 85;
+            });
+            await db.ExecuteAsync("INSERT INTO `score_pins` (`user_id`, `score_type`, `score_id`, `ruleset_id`, `display_order`) VALUES (2, 'solo_score', 2, 0, 0)");
+
+            WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 1", 2, CancellationToken);
+            WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 0", 0, CancellationToken);
+
+            var command = new MarkNonPreservedScoresCommand { RulesetId = 0 };
+            await command.OnExecuteAsync(CancellationToken);
+            WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 1", 2, CancellationToken);
+            WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 0", 0, CancellationToken);
+            WaitForDatabaseState("SELECT `preserve` FROM `scores` WHERE `id` = 2", true, CancellationToken);
+        }
+
+        [Fact]
+        public async Task NonBestScoresRemainPreservedIfAssociatedWithPlaylistItem()
+        {
+            using var db = Processor.GetDatabaseConnection();
+
+            SetScoreForBeatmap(beatmap.beatmap_id, s =>
+            {
+                s.Score.id = 1;
+                s.Score.total_score = 800_000;
+                s.Score.pp = 95;
+            });
+            SetScoreForBeatmap(beatmap.beatmap_id, s =>
+            {
+                s.Score.id = 2;
+                s.Score.total_score = 500_000;
+                s.Score.pp = 85;
+            });
+            await db.ExecuteAsync("INSERT INTO `multiplayer_playlist_item_scores` (`user_id`, `playlist_item_id`, `score_id`) VALUES (2, 1, 2)");
+
+            WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 1", 2, CancellationToken);
+            WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 0", 0, CancellationToken);
+
+            var command = new MarkNonPreservedScoresCommand { RulesetId = 0 };
+            await command.OnExecuteAsync(CancellationToken);
+            WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 1", 2, CancellationToken);
+            WaitForDatabaseState("SELECT COUNT(1) FROM `scores` WHERE `preserve` = 0", 0, CancellationToken);
+            WaitForDatabaseState("SELECT `preserve` FROM `scores` WHERE `id` = 2", true, CancellationToken);
         }
     }
 }
