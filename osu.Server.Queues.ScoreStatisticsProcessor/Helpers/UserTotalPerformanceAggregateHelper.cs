5666bd4b1cbfb90063ae439da67899e44a0b512d
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-06-21 07:59:55 +0200

    Fix mismatched parens in exception message

--- osu.Server.Queues.ScoreStatisticsProcessor/Helpers/UserTotalPerformanceAggregateHelper.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Helpers/UserTotalPerformanceAggregateHelper.cs
@@ -45,10 +45,10 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Helpers
             }
 
             if (double.IsNegative(totalPp) || double.IsNaN(totalPp) || double.IsInfinity(totalPp))
-                throw new InvalidOperationException($"Calculating total PP for user_id:{userId} resulted in invalid value ({totalPp}");
+                throw new InvalidOperationException($"Calculating total PP for user_id:{userId} resulted in invalid value ({totalPp})");
 
             if (double.IsNaN(totalAccuracy) || double.IsInfinity(totalAccuracy))
-                throw new InvalidOperationException($"Calculating total accuracy for user_id:{userId} resulted in invalid value ({totalAccuracy}");
+                throw new InvalidOperationException($"Calculating total accuracy for user_id:{userId} resulted in invalid value ({totalAccuracy})");
 
             // handle floating point precision edge cases.
             totalAccuracy = Math.Clamp(totalAccuracy, 0, 100);
