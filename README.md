0ec8c9692a43aaeb18b5fec77063a42b1217da01
tsunyoku <tsunyoku@gmail.com>
2024-07-15 18:40:42 +0100

    enforce realtime difficulty calculation when using non-default configuration on mods
    add additional support for realtime calculations on non-legacy mods
    
    line endings

--- README.md
+++ README.md
@@ -119,9 +119,9 @@ Whether to process user total stats. Set to `0` to disable processing.
 
 Default is unset (processing enabled).
 
-### REALTIME_DIFFICULTY
+### ALWAYS_REALTIME_DIFFICULTY
 
-Whether to use realtime processing (download beatmaps and compute their difficulty attributes on every processed score), or to rely on database data. Set to `0` to disable processing.
+Whether to use **always** realtime processing (download beatmaps and compute their difficulty attributes on every processed score), or to rely on database data when possible. Set to `0` to disable processing.
 
 Default is unset (processing enabled).
 
--- osu.Server.Queues.ScoreStatisticsProcessor/Stores/BeatmapStore.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Stores/BeatmapStore.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ using osu.Game.Rulesets.Difficulty;
 using osu.Game.Rulesets.Mods;
 using osu.Server.Queues.ScoreStatisticsProcessor.Helpers;
 using osu.Server.Queues.ScoreStatisticsProcessor.Models;
+using StatsdClient;
 using Beatmap = osu.Server.Queues.ScoreStatisticsProcessor.Models.Beatmap;
 
 namespace osu.Server.Queues.ScoreStatisticsProcessor.Stores
@@ -26,7 +28,7 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Stores
     /// </summary>
     public class BeatmapStore
     {
-        private static readonly bool use_realtime_difficulty_calculation = Environment.GetEnvironmentVariable("REALTIME_DIFFICULTY") != "0";
+        private static readonly bool always_use_realtime_difficulty_calculation = Environment.GetEnvironmentVariable("ALWAYS_REALTIME_DIFFICULTY") != "0";
         private static readonly string beatmap_download_path = Environment.GetEnvironmentVariable("BEATMAP_DOWNLOAD_PATH") ?? "https://osu.ppy.sh/osu/{0}";
 
         private readonly ConcurrentDictionary<uint, Beatmap?> beatmapCache = new ConcurrentDictionary<uint, Beatmap?>();
@@ -65,8 +67,16 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Stores
         /// <returns>The difficulty attributes or <c>null</c> if not existing.</returns>
         public async Task<DifficultyAttributes?> GetDifficultyAttributesAsync(Beatmap beatmap, Ruleset ruleset, Mod[] mods, MySqlConnection connection, MySqlTransaction? transaction = null)
         {
-            if (use_realtime_difficulty_calculation)
+            // database attributes are stored using the default mod configurations
+            // if we want to support mods with non-default configurations (i.e non-1.5x rates on DT/NC)
+            // or non-legacy mods which aren't populated into the database
+            // then we must calculate difficulty attributes in real-time.
+            bool mustUseRealtimeDifficulty = mods.Any(m => !m.UsesDefaultConfiguration || !isLegacyMod(m));
+
+            if (always_use_realtime_difficulty_calculation || mustUseRealtimeDifficulty)
             {
+                var stopwatch = Stopwatch.StartNew();
+
                 using var req = new WebRequest(string.Format(beatmap_download_path, beatmap.beatmap_id));
 
                 req.AllowInsecureRequests = true;
@@ -79,7 +89,17 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Stores
                 var workingBeatmap = new StreamedWorkingBeatmap(req.ResponseStream);
                 var calculator = ruleset.CreateDifficultyCalculator(workingBeatmap);
 
-                return calculator.Calculate(mods);
+                var attributes = calculator.Calculate(mods);
+
+                string[] tags =
+                {
+                    $"ruleset:{ruleset.RulesetInfo.OnlineID}",
+                    $"mods:{string.Join("", mods.Select(x => x.Acronym))}"
+                };
+
+                DogStatsd.Timer($"calculate-realtime-difficulty-attributes", stopwatch.ElapsedMilliseconds, tags: tags);
+
+                return attributes;
             }
 
             BeatmapDifficultyAttribute[]? rawDifficultyAttributes;
@@ -108,11 +128,32 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Stores
         }
 
         /// <remarks>
+        /// This method attempts to create a simple solution to deciding if a <see cref="Mod"/> can be considered a "legacy" mod.
+        /// Used by <see cref="GetDifficultyAttributesAsync"/> to decide if the current mod combination's difficulty attributes
+        /// can be fetched from the database.
+        /// </remarks>
+        private static bool isLegacyMod(Mod mod) =>
+            mod is ModNoFail
+                or ModEasy
+                or ModHidden
+                or ModHardRock
+                or ModPerfect
+                or ModSuddenDeath
+                or ModNightcore
+                or ModDoubleTime
+                or ModRelax
+                or ModHalfTime
+                or ModFlashlight
+                or ModCinema
+                or ModAutoplay
+                or ModScoreV2;
+
+        /// <remarks>
         /// This method attempts to choose the best possible set of <see cref="LegacyMods"/> to use for looking up stored difficulty attributes.
         /// The match is not always exact; for some mods that award pp but do not exist in stable
         /// (such as <see cref="ModHalfTime"/>) the closest available approximation is used.
         /// Moreover, the set of <see cref="LegacyMods"/> returned is constrained to mods that actually affect difficulty in the legacy sense.
-        /// The entirety of this workaround is not used / unnecessary if <see cref="use_realtime_difficulty_calculation"/> is <see langword="true"/>.
+        /// The entirety of this workaround is not used / unnecessary if <see cref="always_use_realtime_difficulty_calculation"/> is <see langword="true"/>.
         /// </remarks>
         private static LegacyMods getLegacyModsForAttributeLookup(Beatmap beatmap, Ruleset ruleset, Mod[] mods)
         {
