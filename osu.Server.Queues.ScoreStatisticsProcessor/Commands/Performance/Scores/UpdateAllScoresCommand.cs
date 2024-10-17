94989be1d518d1f886fbe8d2bb8c8ae72a0a5488
Dean Herbert <pe@ppy.sh>
2024-10-17 20:34:07 +0900

    Batch differently (and fix up various code quality issues)

--- osu.Server.Queues.ScoreStatisticsProcessor/Commands/Performance/Scores/UpdateAllScoresCommand.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Commands/Performance/Scores/UpdateAllScoresCommand.cs
@@ -2,28 +2,27 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
 using McMaster.Extensions.CommandLineUtils;
 using osu.Server.QueueProcessor;
-using osu.Server.Queues.ScoreStatisticsProcessor.Helpers;
+using osu.Server.Queues.ScoreStatisticsProcessor.Models;
 
 namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Performance.Scores
 {
     [Command(Name = "all", Description = "Computes pp of all scores from all users.")]
     public class UpdateAllScoresCommand : PerformanceCommand
     {
-        private const int max_users_per_query = 10000;
+        private const int max_scores_per_query = 50000;
 
         [Option(Description = "Score ID to start processing from.")]
         public ulong From { get; set; }
 
         protected override async Task<int> ExecuteAsync(CancellationToken cancellationToken)
         {
-            LegacyDatabaseHelper.RulesetDatabaseInfo databaseInfo = LegacyDatabaseHelper.GetRulesetSpecifics(RulesetId);
-
             ulong? totalCount;
             ulong currentScoreId = From;
 
@@ -32,36 +31,48 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Performance.Scores
                 totalCount = await db.QuerySingleAsync<ulong>("SELECT MAX(id) FROM scores");
             }
 
-            Console.WriteLine($"Processing all scores starting from {currentScoreId}");
+            Console.WriteLine($"Processing all {totalCount} scores starting from {currentScoreId}");
 
             int processedCount = 0;
 
             while (!cancellationToken.IsCancellationRequested)
             {
-                ulong[] scoreIds;
+                SoloScore[] scores;
 
                 using (var db = DatabaseAccess.GetConnection())
                 {
-                    scoreIds = (await db.QueryAsync<ulong>($"SELECT id FROM scores WHERE `id` > @ScoreId ORDER BY `id` LIMIT @Limit", new
+                    scores = (await db.QueryAsync<SoloScore>("SELECT * FROM scores WHERE `id` > @ScoreId ORDER BY `id` LIMIT @Limit", new
                     {
                         ScoreId = currentScoreId,
-                        Limit = max_users_per_query
+                        Limit = max_scores_per_query
                     })).ToArray();
                 }
 
-                if (scoreIds.Length == 0)
+                if (scores.Length == 0)
                     break;
 
-                await ProcessPartitioned(scoreIds, async id =>
-                {
-                    using (var db = DatabaseAccess.GetConnection())
-                        await ScoreProcessor.ProcessScoreAsync(id, db);
+                await Task.WhenAll(Partitioner
+                                   .Create(scores)
+                                   .GetPartitions(Threads)
+                                   .Select(async partition =>
+                                   {
+                                       using (var connection = DatabaseAccess.GetConnection())
+                                       using (partition)
+                                       {
+                                           while (partition.MoveNext())
+                                           {
+                                               if (cancellationToken.IsCancellationRequested)
+                                                   return;
 
-                    if (Interlocked.Increment(ref processedCount) % 1000 == 0)
-                        Console.WriteLine($"Processed {processedCount} of {totalCount}");
-                }, cancellationToken);
+                                               await ScoreProcessor.ProcessScoreAsync(partition.Current, connection);
 
-                currentScoreId = scoreIds.Max();
+                                               if (Interlocked.Increment(ref processedCount) % 1000 == 0)
+                                                   Console.WriteLine($"Processed {processedCount} of {totalCount}");
+                                           }
+                                       }
+                                   }));
+
+                currentScoreId = scores.Last().id;
             }
 
             return 0;
