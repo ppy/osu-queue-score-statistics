74fd577fd07f4763bb46652d5beb7d66e1d0c3ed
Dean Herbert <pe@ppy.sh>
2025-02-27 16:39:02 +0900

    Fix tests

--- osu.Server.Queues.ScoreStatisticsProcessor.Tests/PerformanceProcessorTests.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor.Tests/PerformanceProcessorTests.cs
@@ -63,7 +63,6 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
             {
                 attr.AimDifficulty = 3;
                 attr.SpeedDifficulty = 3;
-                attr.OverallDifficulty = 3;
             });
 
             SetScoreForBeatmap(TEST_BEATMAP_ID, score =>
@@ -75,8 +74,8 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
                 score.Score.preserve = true;
             });
 
-            // 160pp from the single score above + 2pp from playcount bonus
-            WaitForDatabaseState("SELECT rank_score FROM osu_user_stats WHERE user_id = 2", 162, CancellationToken);
+            // 158pp from the single score above + 2pp from playcount bonus
+            WaitForDatabaseState("SELECT rank_score FROM osu_user_stats WHERE user_id = 2", 160, CancellationToken);
 
             // purposefully identical to score above, to confirm that you don't get pp for two scores on the same map twice
             SetScoreForBeatmap(TEST_BEATMAP_ID, score =>
@@ -88,8 +87,8 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
                 score.Score.preserve = true;
             });
 
-            // 160pp from the single score above + 4pp from playcount bonus
-            WaitForDatabaseState("SELECT rank_score FROM osu_user_stats WHERE user_id = 2", 164, CancellationToken);
+            // 158pp from the single score above + 4pp from playcount bonus
+            WaitForDatabaseState("SELECT rank_score FROM osu_user_stats WHERE user_id = 2", 162, CancellationToken);
         }
 
         /// <summary>
@@ -113,9 +112,6 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
                 attr.StaminaDifficulty = 3;
                 attr.RhythmDifficulty = 3;
                 attr.ColourDifficulty = 3;
-                attr.PeakDifficulty = 3;
-                attr.GreatHitWindow = 15;
-                attr.OkHitWindow = 50;
             }, mode: 1);
 
             SetScoreForBeatmap(TEST_BEATMAP_ID, score =>
@@ -129,8 +125,8 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Tests
                 score.Score.preserve = true;
             });
 
-            // 374pp from the single score above + 2pp from playcount bonus
-            WaitForDatabaseState("SELECT rank_score FROM osu_user_stats_taiko WHERE user_id = 2", 376, CancellationToken);
+            // 218 from the single score above + 2pp from playcount bonus
+            WaitForDatabaseState("SELECT rank_score FROM osu_user_stats_taiko WHERE user_id = 2", 220, CancellationToken);
         }
 
         [Fact]
