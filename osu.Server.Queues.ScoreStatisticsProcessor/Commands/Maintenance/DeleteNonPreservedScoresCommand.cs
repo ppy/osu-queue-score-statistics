aec83fd7b01ea951f1ee4b42331491d0ee559d40
Dean Herbert <pe@ppy.sh>
2026-02-12 16:51:31 +0900

    Batch deletions in smaller batches
    This allows proper cancellation. Previously it wouldn't work because of
    the way dapper buffers results (doesn't pass cancellation the whole way,
    only on the reader part).

--- osu.Server.Queues.ScoreStatisticsProcessor/Commands/Maintenance/DeleteNonPreservedScoresCommand.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Commands/Maintenance/DeleteNonPreservedScoresCommand.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -36,37 +37,44 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Commands.Maintenance
 
                 await deleteCommand.PrepareAsync(cancellationToken);
 
-                var scores = await readConnection.QueryAsync<SoloScore>(new CommandDefinition(
-                    $"SELECT * FROM `scores` WHERE `preserve` = 0 AND `unix_updated_at` < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL {preserve_hours} HOUR))", flags: CommandFlags.None, cancellationToken: cancellationToken));
-
-                foreach (var score in scores)
+                while (true)
                 {
-                    if (cancellationToken.IsCancellationRequested)
+                    var scores = await readConnection.QueryAsync<SoloScore>(new CommandDefinition(
+                        $"SELECT * FROM `scores` WHERE `preserve` = 0 AND `unix_updated_at` < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL {preserve_hours} HOUR)) LIMIT 1000",
+                        cancellationToken: cancellationToken));
+
+                    if (!scores.Any())
                         break;
 
-                    Console.WriteLine($"Deleting score {score.id}...");
-                    scoreId.Value = score.id;
-                    await deleteCommand.ExecuteNonQueryAsync(cancellationToken);
+                    Console.WriteLine($"Processing next batch of {scores.Count()} scores");
 
-                    // TODO: check pins
-
-                    if (score.has_replay)
+                    foreach (var score in scores)
                     {
-                        Console.WriteLine("* Removing replay from S3...");
-                        var deleteResult = await s3.DeleteObjectAsync(S3.REPLAYS_BUCKET, score.id.ToString(CultureInfo.InvariantCulture), cancellationToken);
+                        if (cancellationToken.IsCancellationRequested)
+                            break;
 
-                        switch (deleteResult.HttpStatusCode)
+                        Console.WriteLine($"Deleting score {score.id}...");
+                        scoreId.Value = score.id;
+                        await deleteCommand.ExecuteNonQueryAsync(cancellationToken);
+
+                        if (score.has_replay)
                         {
-                            case HttpStatusCode.NoContent:
-                                // below wording is intentionally very roundabout, because s3 does not actually really seem to produce the types of error you'd expect.
-                                // for instance, even if you request removal of a nonexistent object, it'll just throw a 204 No Content back
-                                // with no real way to determine whether it actually even did anything.
-                                Console.WriteLine("* Deletion request completed without error.");
-                                break;
+                            Console.WriteLine("* Removing replay from S3...");
+                            var deleteResult = await s3.DeleteObjectAsync(S3.REPLAYS_BUCKET, score.id.ToString(CultureInfo.InvariantCulture), cancellationToken);
 
-                            default:
-                                await Console.Error.WriteLineAsync($"* Received unexpected status code when attempting to delete replay: {deleteResult.HttpStatusCode}.");
-                                break;
+                            switch (deleteResult.HttpStatusCode)
+                            {
+                                case HttpStatusCode.NoContent:
+                                    // below wording is intentionally very roundabout, because s3 does not actually really seem to produce the types of error you'd expect.
+                                    // for instance, even if you request removal of a nonexistent object, it'll just throw a 204 No Content back
+                                    // with no real way to determine whether it actually even did anything.
+                                    Console.WriteLine("* Deletion request completed without error.");
+                                    break;
+
+                                default:
+                                    await Console.Error.WriteLineAsync($"* Received unexpected status code when attempting to delete replay: {deleteResult.HttpStatusCode}.");
+                                    break;
+                            }
                         }
                     }
                 }
